6514476bcdc404bdc0c4fc8520ebe6567a8200fe
Ubuntu <ubuntu@ip-172-31-36-75.eu-north-1.compute.internal>
2025-04-14 05:10:23 +0000

    Commit from EC2

--- ScubaDiveIn/ScubaDiveIn/settings.py
+++ ScubaDiveIn/ScubaDiveIn/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2n-3#t%!fum0)m#+*mdafc9*#am$zprt502llr2g2jo3rt#e0$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["13.61.19.80", "scubadivein.in"]
 
 
 # Application definition
